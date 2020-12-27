006cab79fc9c971fb3e1ca34c5695b350a8d1df4
hrazzagh <hrazzagh@gmail.com>
2020-12-27 03:10:18 +0100

    Initial commit.

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
 /*
-Notes: 
+Notes:
 'data' is lazily imported from the html
 'seedrandom' is also imported from html. it gives deterministic random #s based on a seed set in fire()
 */
 
 
 var wordsSelected = [];
-var teams = [];
+var cells = [];
 var NUMBER_OF_WORDS = 25;
 var spyMasterMode = false;
 var sessionData = [];
 var customData = [];
 
-var COLOR_RED = "#ff0000";
-var COLOR_YELLOW = "#ffff00";
-var COLOR_BLUE = "#00eeee";
-var COLOR_BLACK = "#808080";
-var COLOR_GREEN = "#009000";
+// these are the names of classes that will be applied to cells
+// edit these styles in stlyes/styles.css
+var RED_TEAM = "red-team";
+var BLUE_TEAM = "blue-team";
+var NEUTRAL = "neutral";
+var BOMB = "bomb";
+var GUESSED = "guessed";
+var startTeam;
 
 //init
-$("#seed").keyup(function() {
-	fire();
+$("#seed").keyup(function () {
+    fire();
 });
 
-$("#gameMode").change(function() {
-	fire();
+$("#gameMode").change(function () {
+    fire();
 });
 
 
@@ -32,209 +35,229 @@ $("#seed").val(Math.floor(Math.random() * 1000));
 fire();
 
 function fire() {
-	//get seed and set the seed for randomizer
-	var seed = document.getElementById("seed").value;
-	Math.seedrandom(seed.toLowerCase());
+    //get seed and set the seed for randomizer
+    var seed = document.getElementById("seed").value;
+    Math.seedrandom(seed.toLowerCase());
 
-	var option = $('#gameMode :selected').val();
-	switch (option) {
-		case 'spanish':
-			sessionData = spanishData.slice(0);
-			break;
-		case '2knouns':
-			sessionData = data.slice(0);
-			break;
-		case 'movies':
-			sessionData = movieData.slice(0);
-			break;
-		case 'custom':
-			if (customData.length === 0) {
-				var customWordList = prompt("Please enter custom word list. The list will be saved until your refresh your browser. (The words MUST be delimanted by spaces). eg: cat dog mouse", "Enter words here");
-				customData = customWordList.split(' ');
-			}
-			sessionData = customData.slice(0);
-			break;
-		default:
-			sessionData = defaultData.slice(0);
-	}
+    var option = $('#gameMode :selected').val();
+    switch (option) {
+        case 'spanish':
+            sessionData = spanishData.slice(0);
+            break;
+        case '2knouns':
+            sessionData = data.slice(0);
+            break;
+        case 'movies':
+            sessionData = movieData.slice(0);
+            break;
+        case 'custom':
+            if (customData.length === 0) {
+                var customWordList = prompt("Please enter custom word list. The list will be saved until your refresh your browser. (The words MUST be delimanted by spaces). eg: cat dog mouse", "Enter words here");
+                customData = customWordList.split(' ');
+            }
+            sessionData = customData.slice(0);
+            break;
+        default:
+            sessionData = defaultData.slice(0);
+    }
 
-	wordsSelected = [];
-	teams = [];
-	spyMasterMode = false;
-	document.getElementById("board").innerHTML = "";
+    wordsSelected = [];
+    cells = [];
+    spyMasterMode = false;
+    document.getElementById("board").innerHTML = "";
 
-	//fire new board
-	updateScore();
-	createNewGame();
+    //fire new board
+    updateScore();
+    createNewGame();
 }
 
 //not used, but probably useful at some point
 function removeItem(array, index) {
-	if (index > -1) {
-		// console.log("index: " + index + ", word: " + array[index] + " removed.");
-		array.splice(index, 1);
-	}
+    if (index > -1) {
+        // console.log("index: " + index + ", word: " + array[index] + " removed.");
+        array.splice(index, 1);
+    }
 }
 
 function createNewGame() {
-	var trs = [];
-	for (var i = 0; i < NUMBER_OF_WORDS; i++) {
-		if (!trs[i % 5]) {
-			trs[i % 5] = "";
-		}
-		var randomNumber = Math.floor(Math.random() * sessionData.length);
-		var word = sessionData[randomNumber];
+    var trs = [];
+    for (var i = 0; i < NUMBER_OF_WORDS; i++) {
+        if (!trs[i % 5]) {
+            trs[i % 5] = "";
+        }
+        var randomNumber = Math.floor(Math.random() * sessionData.length);
+        var word = sessionData[randomNumber];
+        removeItem(sessionData, randomNumber);
+        wordsSelected.push(word);
+        trs[i % 5] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><span class=\"ada\"></span>" + word + "</a></div></div>";
+    }
+    //<a href="#"><span class="ada">Washington stimulates economic growth </span>Read me</a>
+    for (var i = 0; i < trs.length; i++) {
+        document.getElementById("board").innerHTML += '<div class="row">' + trs[i] + '</div>'
+    }
 
-		var words = word.split('#');
+    // create array of word cells
+    for (var i = 0; i < 8; i++) {
+        cells.push(RED_TEAM);
+        cells.push(BLUE_TEAM);
+    }
 
-		removeItem(sessionData, randomNumber);
-		wordsSelected.push(word);
-		// trs[i % 5] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><span class=\"ada\"></span>" + words[0] + "</a>" + "<br/>" + "(" + words[1] + ")" + "</div></div>";
-		trs[i % 5] += "<div class=\"word\" id=\'" + i + "\' onclick=\"clicked(\'" + i + "\')\"><div><a href=\"#\"><span class=\"ada\"></span>" + word + "</a></div></div>";
-	}
-	//<a href="#"><span class="ada">Washington stimulates economic growth </span>Read me</a>
-	for (var i = 0; i < trs.length; i++) {
-		document.getElementById("board").innerHTML += '<div class="row">' + trs[i] + '</div>'
-	}
+    // one team gets an extra cell
+    if (Math.floor(Math.random() * data.length) % 2 === 0) {
+        cells.push(RED_TEAM);
+        startTeam = RED_TEAM
+        $('#score-container').addClass('redStart').removeClass('blueStart');
 
-	//create teams
-	for (var i = 0; i < 8; i++) {
-		teams.push(COLOR_RED);
-		teams.push(COLOR_BLUE);
-	}
+    } else {
+        cells.push(BLUE_TEAM);
+        startTeam = BLUE_TEAM
+        $('#score-container').addClass('blueStart').removeClass('redStart');
+    }
 
-	// one extra for one of the teams
-	if (Math.floor(Math.random() * data.length) % 2 === 0) {
-		teams.push(COLOR_RED);
-		// document.getElementById("team").style.color = COLOR_RED;
-		// document.getElementById("team").innerHTML = "RED";
-		$('#board').addClass('redStarts').removeClass('blueStarts');
+    // add neutrals
+    for (var i = 0; i < 7; i++) {
+        cells.push(NEUTRAL);
+    }
 
-	} else {
-		teams.push(COLOR_BLUE);
-		// document.getElementById("team").style.color = COLOR_BLUE;
-		// document.getElementById("team").innerHTML = "BLUE";
-		$('#board').addClass('blueStarts').removeClass('redStarts');
-	}
+    // push the bomb
+    cells.push(BOMB)
 
-	// add neturals 
-	for (var i = 0; i < 7; i++) {
-		teams.push(COLOR_YELLOW);
-	}
+    //shuffle cells
+    shuffle(cells);
 
-	// push the assasin
-	teams.push(COLOR_BLACK)
-
-	//shuffle teams
-	shuffle(teams);
-
-	updateScore();
+    updateScore();
 }
 
 function clicked(value) {
-	if (spyMasterMode) {
-		//spymaster mode
-		document.getElementById(value).style.backgroundColor = COLOR_GREEN;
-	} else {
-		//guessers mode
-		var word = wordsSelected[value];
-		if (document.getElementById("confirm").checked) {
-			if (window.confirm("Are sure you want to select '" + word + "'?")) {
-				document.getElementById(value).style.backgroundColor = teams[value];
-				if (teams[value] == "black") {
-					document.getElementById(value).style.color = "white";
-				}
-			}
-		} else {
-			document.getElementById(value).style.backgroundColor = teams[value];
-			if (teams[value] == "black") {
-				document.getElementById(value).style.color = "white";
-			}
-		}
-	}
-	updateScore();
+    var elem = $("#" + value);
+
+    if (spyMasterMode) {
+        //spymaster mode
+        if (elem.hasClass(GUESSED)) {
+            elem.removeClass(GUESSED);
+        } else {
+            elem.addClass(GUESSED);
+        }
+    } else {
+        //guessers mode
+        var word = wordsSelected[value];
+        if (document.getElementById("confirm").checked) {
+            if (window.confirm("Are sure you want to select '" + word + "'?")) {
+                doStuff();
+            }
+        } else {
+            doStuff();
+        }
+    }
+
+    updateScore();
+}
+
+function doStuff() {
+    if (elem.data('guessed')) {
+        elem.removeClass(cells[value]);
+        elem.data('guessed', false);
+    } else {
+        elem.addClass(cells[value]);
+        elem.data('guessed', true);
+    }
 }
 
 function updateScore() {
-	var blueScore = 9;
-	var redScore = 9;
-	if (spyMasterMode) {
-		blueScore = 0;
-		redScore = 0;
-		$('div.word').each(function() {
-			var color = $(this).css('background-color');
-			if (color === 'rgb(0, 238, 238)') {
-				blueScore++;
-			}
-			if (color === 'rgb(255, 0, 0)') {
-				redScore++;
-			}
-		});
-	} else {
-		$('div.word').each(function() {
-			var color = $(this).css('background-color');
-			if (color === 'rgb(0, 238, 238)') {
-				blueScore--;
-			}
-			if (color === 'rgb(255, 0, 0)') {
-				redScore--;
-			}
-		});
+    var blueScore = 9;
+    var redScore = 9;
+    if (spyMasterMode) {
+        blueScore = 0;
+        redScore = 0;
+        $('div.word').each(function () {
+            if ($(this).hasClass(BLUE_TEAM)) {
+                blueScore++;
+            }
+            if ($(this).hasClass(RED_TEAM)) {
+                redScore++;
+            }
+        });
+    } else {
+        $('div.word').each(function () {
+            if ($(this).hasClass(BLUE_TEAM)) {
+                blueScore--;
+            }
+            if ($(this).hasClass(RED_TEAM)) {
+                redScore--;
+            }
+        });
 
-		if ($('.redStarts').length === 1) {
-			blueScore--;
-		} else {
-			redScore--;
-		}
-	}
-	$('#redScore').text(redScore);
-	$('#blueScore').text(blueScore);
-	if(redScore === 0){
-		$('#redScore').text('Winner!');
-	}
-	if(blueScore === 0){
-		$('#blueScore').text('Winner!');
-	}
+        if (startTeam == RED_TEAM) {
+            blueScore--;
+        } else {
+            redScore--;
+        }
+    }
+    $('#redScore').text(redScore);
+    $('#blueScore').text(blueScore);
+    if (redScore === 0) {
+        $('#redScore').text('Winner!');
+    }
+    if (blueScore === 0) {
+        $('#blueScore').text('Winner!');
+    }
 }
 
 function spyMaster() {
-	//TODO: randomize or organize tiles for easier comparing
-	spyMasterMode = true;
-	for (var i = 0; i < NUMBER_OF_WORDS; i++) {
-		document.getElementById(i).style.backgroundColor = teams[i];
-		if (teams[i] == "black") {
-			document.getElementById(i).style.color = "white";
-		}
-	}
+    //TODO: randomize or organize tiles for easier comparing
+    var elem;
+    if (!spyMasterMode) {
+        spyMasterMode = true;
+        for (var i = 0; i < NUMBER_OF_WORDS; i++) {
+            elem = $("#" + i);
+            elem.addClass(cells[i]);
+            if (elem.data('guessed')) {
+                elem.addClass(GUESSED);
+            }
+        }
+    } else {
+        spyMasterMode = false;
+
+        for (var i = 0; i < NUMBER_OF_WORDS; i++) {
+            elem = $("#" + i);
+            var toRemove = [GUESSED];
+
+            if (!elem.data('guessed')) {
+                toRemove.push(cells[i]);
+            }
+
+            elem.removeClass(toRemove.join(' '));
+        }
+    }
 }
 
 function shuffle(array) {
-	var currentIndex = array.length,
-		temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-		// And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-	return array;
+    return array;
 }
 
 //enable pressing 'Enter' on seed field
-document.getElementById('seed').onkeypress = function(e) {
-	if (!e) e = window.event;
-	var keyCode = e.keyCode || e.which;
-	if (keyCode == '13') {
-		// Enter pressed
-		fire();
-		return false;
-	}
+document.getElementById('seed').onkeypress = function (e) {
+    if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13') {
+        // Enter pressed
+        fire();
+        return false;
+    }
 }
